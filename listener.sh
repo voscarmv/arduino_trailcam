6c6554048267fd6c9124aa217c85b687338cca34
Oscar Mier <voscarmv@gmail.com>
2025-04-23 18:13:15 -0500

    Update listener.sh

--- listener.sh
+++ listener.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# A shell script that listens to Arduino broadcasting the state of the motion sensor
-# For now it only receives ASCII "Hello\n" from the sketch provided, and processes each line by appendoing " World!"
-# This is supposed to show the principle that we can process whatever Arduino transmits in a shell
-# Next up is connecting this to a USB webcam through ffmpeg with something like ffmpeg -f video4linux2 -i /dev/video0 -vframes 1 -y out.jpg
-
 stty -F /dev/ttyACM0 9600
-tail -f /dev/ttyACM0 | while read line ; do echo $line World!  ; done
+tail -f /dev/ttyACM0 | while read line ; do
+  if test $line = "Movement detected" ; then
+    ffmpeg -f video4linux2 -i /dev/video0 -vframes 1 -y out.jpg # Take a picture if movement detected
+    # Then send it to a database here.
+  fi
+done
