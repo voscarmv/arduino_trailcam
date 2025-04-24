706c2500fcb019c704be3ae32f4f8c92975052c2
Oscar Mier <voscarmv@gmail.com>
2025-04-24 13:19:17 -0500

    Update listener.sh

--- listener.sh
+++ listener.sh
@@ -5,5 +5,6 @@ tail -f /dev/ttyACM0 | while read line ; do
   if test $line = "Movement detected" ; then
     ffmpeg -f video4linux2 -i /dev/video0 -vframes 1 -y out.jpg # Take a picture if movement detected
     # Then send it to a database here.
+    printf '\x01' > /dev/ttyACM0 # Toggle standby state off in the Arduino so it keeps detecting movement
   fi
 done
